1f3d1429fcfd03aceb884629da2495dd1cea0bbb
Kenrick <kenrick95@gmail.com>
2020-04-01 23:17:06 +0800

    Add browser unsupported message

---
+++ nomodule.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('unsupported').style.display = 'block';
+  document.getElementById('controls').style.display = 'none';
+  document.getElementById('chart').style.display = 'none';
+});
