e1c978b5779e3c304b5d79c003ffd340a683ad59
Kenrick <kenrick95@gmail.com>
2020-04-01 22:34:29 +0800

    Select question plus stylize

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
 import Chart from 'https://dev.jspm.io/chart.js';
 import { DATA } from './data.js';
 
-// TODO: Provide option to select other questions
-
 function main() {
   const ctx = document.getElementById('chart');
-//   console.log('DATA', DATA);
-  const selectedQuestionId = 0;
-  const selectedQuestion = DATA.questions.find((question) => {
-    return question.qID === selectedQuestionId;
-  });
-  const answersForSelectedQuestion = DATA.answers.filter((answer) => {
-    return answer.qID === selectedQuestionId;
-  });
-  const countries = DATA.countries;
-  answersForSelectedQuestion.sort((a, b) => {
-    return a.cntryID - b.cntryID;
-  });
+  const questionSelectEl = document.getElementById('question');
+
+  let state = {
+    selectedQuestionId: 0
+  };
+
+  function setState(newPartialState) {
+    state = {
+      ...state,
+      ...newPartialState
+    };
+    renderChart();
+  }
+  //   console.log('DATA', DATA);
 
   const chart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: countries.map((country) => country.title),
-      datasets: [
-        {
-          label: 'Rural',
-          // TODO: Provide option to select other "Living on" group
-          data: answersForSelectedQuestion.map((answer) => answer.values[0][0]),
-          borderWidth: 1,
-          backgroundColor: '#ffcc99'
-        },
-        {
-          label: 'Urban',
-          data: answersForSelectedQuestion.map((answer) => answer.values[0][1]),
-          borderWidth: 1,
-          backgroundColor: '#ff9900'
-        }
-      ]
+      labels: [],
+      dataset: []
     },
     options: {
       scales: {
@@ -50,9 +36,83 @@ function main() {
       },
       title: {
         display: true,
-        text: [selectedQuestion.title, selectedQuestion.title2]
+        text: ''
       }
     }
   });
+
+  function renderChart() {
+    const { selectedQuestionId } = state;
+    const selectedQuestion = DATA.questions.find((question) => {
+      return question.qID === selectedQuestionId;
+    });
+    const answersForSelectedQuestion = DATA.answers.filter((answer) => {
+      return answer.qID === selectedQuestionId;
+    });
+    const countries = DATA.countries;
+    answersForSelectedQuestion.sort((a, b) => {
+      return a.cntryID - b.cntryID;
+    });
+
+    chart.data.labels = countries.map((country) => country.title);
+    chart.data.datasets = [
+      {
+        label: 'Rural',
+        // TODO: Provide option to select other "Living on" group
+        data: answersForSelectedQuestion.map((answer) => answer.values[0][0]),
+        borderWidth: 1,
+        backgroundColor: '#ffcc99'
+      },
+      {
+        label: 'Urban',
+        data: answersForSelectedQuestion.map((answer) => answer.values[0][1]),
+        borderWidth: 1,
+        backgroundColor: '#ff9900'
+      }
+    ];
+
+    chart.options.title.text = [
+      selectedQuestion.title,
+      selectedQuestion.title2
+    ];
+    chart.update();
+  }
+
+  function renderQuestions() {
+    questionSelectEl.innerHTML = '';
+    const questionsGrouped = DATA.categories.map((category) => {
+      return {
+        cID: category.cID,
+        name: category.name,
+        questions: DATA.questions.filter((question) => {
+          return question.cID === category.cID;
+        })
+      };
+    });
+
+    for (const group of questionsGrouped) {
+      const questionGroupEl = document.createElement('optgroup');
+      questionGroupEl.label = group.name;
+      questionGroupEl.dataset.cid = group.cID;
+      for (const question of group.questions) {
+        const questionEl = document.createElement('option');
+        questionEl.value = question.qID;
+        questionEl.textContent = question.title;
+        questionGroupEl.appendChild(questionEl);
+      }
+      questionSelectEl.appendChild(questionGroupEl);
+    }
+  }
+
+  function handleQuestionSelect(event) {
+    const selectedQuestionId = parseInt(event.target.value, 10);
+    setState({
+      selectedQuestionId
+    });
+  }
+  questionSelectEl.addEventListener('change', handleQuestionSelect);
+
+  renderQuestions();
+  renderChart(0);
 }
 main();
